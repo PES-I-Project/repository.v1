97cf7f1e3404387e52a7babb973b6af800c2c9bf
Joao-Biederman <joaobiedetman@gmail.com>
2023-07-14 20:22:44 -0300

    creating client with Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Example from './pages/example';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<HomePage />} />
+          // <Route path='/novo-post' element={<NovoPost />} />
+          // <Route path='/:id' element={<BlogPost />} />
+          // <Route path='/:id/editar' element={<EditarPost />} />
+          // <Route path='/sobre-nos' element={<SobreNos />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
